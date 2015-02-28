b78d7b99bf7b11e8b33d629b8f214d78c29608f2
glocascio <gene.locascio@gmail.com>
2015-02-28 12:21:46 -0800

    initial build

---
+++ README.md
@@ -0,0 +1,7 @@
+//Shuffle and then sort the various pigs by age and variety!
+
+//I found some very helpful posts and topics on Google for this project. 
+
+//In particular, credit for the sorting animation function:  http://stackoverflow.com/questions/9893134/appendto-animation
+
+//-GLocascio
---
+++ shufflePigs.js
@@ -0,0 +1,34 @@
+$('#shufflePigs').on('click', function() {
+  var shuffle = function(m) {
+    var rand, $rand;
+    rand = Math.floor(Math.random() * m--);
+    $('img:eq(' + m + ')').
+      after($('img:eq(' + rand + ')')).
+      insertBefore($('img:eq(' + rand + ')'))
+    if(m) {
+      setTimeout(shuffle, 125, m);
+    }
+  };
+
+  shuffle($('img').length);
+});
+
+$.fn.animateAppendTo = function(sel, speed) {
+    var $this = this,
+        newEle = $this.clone(true).appendTo(sel),
+        newPos = newEle.position();
+    newEle.hide();
+    $this.css('position', 'absolute').animate(newPos, speed, function() {
+        newEle.show();
+        $this.remove();
+    });
+    return newEle;
+};
+
+$('#pigSort').on('click', function() {
+  $('.bulldog').animateAppendTo('#bullDogs').show('slow');
+  $('.bullpup').animateAppendTo('#bullPups').show('slow');
+  $('.pig').animateAppendTo('#Pigs').show('slow');
+  $('.bigpig').animateAppendTo('#bigPigs').show('slow');
+  $('.BOTH').animateAppendTo('#both').show('slow');
+});
